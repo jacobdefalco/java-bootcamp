ec7a5aac58b2d55dad330850c54fc5371e2da48d
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-07-19 08:54:23 -0400

    init commit

---
+++ java-bootcamp/src/assignments/CoinCount.java
@@ -0,0 +1,25 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class CoinCount {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        System.out.println("How many pennies are you inputting?");
+        double pennies = myObj.nextDouble(); // Read user input
+        System.out.println("How many nickels are you inputting?");
+        double nickels = myObj.nextDouble();
+        System.out.println("How many dimes are you inputting?");
+        double dimes = myObj.nextDouble();
+        System.out.println("How many quarters are you inputting?");
+        double quarters = myObj.nextDouble();
+
+        double total = (pennies) + (nickels * 5) + (dimes * 10) + (quarters * 25);
+        double cents = total % 100;
+        double dollars = (total - cents) / 100;
+
+        System.out
+                .println("****THANK YOU**** We will now credit your account with: " + dollars + " dollars and " + cents
+                        + " cents"); // Output user input
+    }
+}
---
+++ java-bootcamp/src/assignments/MyProfile.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.println("""
+                Name: Jacob DeFalco
+                ***
+                Campus: Pittsburgh, PA
+                ***
+                Career Goal: Cyber Security Technician
+                ***
+                Age: 26
+                ***
+                Brief Intro: I'm just a kid (Simple Plan reference) just trying to make it in this crazy world
+                """);
+    }
+}
