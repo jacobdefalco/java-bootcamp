847766ba2b525aeb43b7d4f78372024b290694f8
Jacob DeFalco <jake.defalco97@gmail.com>
2023-07-21 00:51:20 -0400

    im lost and giving up until tomorrow

--- java-bootcamp/src/assignments/GradeBook.java
+++ java-bootcamp/src/assignments/GradeBook.java
@@ -13,37 +13,48 @@ public class GradeBook {
         }
         Scanner GradeBookObj = new Scanner(System.in);
         System.out.println("************");
+
+        String YorN;
+
         System.out.println("Would you like to input grades? (y/n)");
-        String proceed = GradeBookObj.nextLine();
-        if (proceed != "y" || proceed != "n") {
-            System.out.println("y/n needed");
+        YorN = (GradeBookObj.nextLine()).toLowerCase();
+
+        if (YorN == "n") {
+            System.out.println("Alright, see ya!");
         }
-        String proceed2 = GradeBookObj.nextLine();
-        switch (proceed2) {
-            case "n":
-                System.out.println("See ya!");
-                break;
-            case "y":
-                System.out.println("Student name:");
-                String studentName = GradeBookObj.nextLine();
-                System.out.println("Course name:");
-                String courseName = GradeBookObj.nextLine();
-                System.out.println("Midterm grade (Needs to be between 0-100):");
-                int midtermGrade = GradeBookObj.nextInt();
-                System.out.println("Final grade (Needs to be between 0-100):");
-                int finalGrade = GradeBookObj.nextInt();
-                System.out.println("Project grade (Needs to be between 0-100)");
-                int projectGrade = GradeBookObj.nextInt();
-                double averageGrade = (midtermGrade + finalGrade + projectGrade) / 3;
-                System.out.println("********Grade Results********");
-                System.out.println("Student Name: " + studentName);
-                System.out.println("Course Name: " + courseName);
-                System.out.println("Midterm Exam Grade: " + midtermGrade);
-                System.out.println("Final Exam Grade: " + finalGrade);
-                System.out.println("Average Exam Grade: " + averageGrade);
-                break;
+
+        if (YorN == "y") {
+            System.out.println("Student name:");
+            String studentName = GradeBookObj.nextLine();
+            System.out.println("Course name:");
+            String courseName = GradeBookObj.nextLine();
+
+            int midtermGrade;
+            int finalGrade;
+            int projectGrade;
+            do {
+                System.out.println("Midterm grade (Must to be between 0-100):");
+                midtermGrade = GradeBookObj.nextInt();
+            } while (midtermGrade < 0 || midtermGrade > 100);
+            do {
+                System.out.println("Final grade (Must to be between 0-100):");
+                finalGrade = GradeBookObj.nextInt();
+            } while (finalGrade < 0 || finalGrade > 100);
+            do {
+                System.out.println("Project grade (Must to be between 0-100):");
+                projectGrade = GradeBookObj.nextInt();
+            } while (projectGrade < 0 || projectGrade > 100);
+
+            double averageGrade = (midtermGrade + finalGrade + projectGrade) / 3;
+
+            System.out.println("********Grade Results********");
+            System.out.println("Student Name: " + studentName);
+            System.out.println("Course Name: " + courseName);
+            System.out.println("Midterm Exam Grade: " + midtermGrade);
+            System.out.println("Final Exam Grade: " + finalGrade);
+            System.out.println("Project Grade: " + projectGrade);
+            System.out.println("Average Exam Grade: " + averageGrade);
         }
         GradeBookObj.close();
     }
-
 }
