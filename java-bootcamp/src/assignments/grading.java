1fbd9a2c085830634207d735d675b96864d776ca
Jacob DeFalco <jacob.defalco97@gmail.com>
2023-07-20 22:13:43 -0400

    init commit

---
+++ java-bootcamp/src/assignments/grading.java
@@ -0,0 +1,25 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class grading {
+    public static void main(String[] args) {
+        Scanner GradeObj = new Scanner(System.in);
+        System.out.println("What is your first name?");
+        String UsersName = GradeObj.nextLine();
+        String username = UsersName.substring(0, 3).toLowerCase();
+        System.out.println("Your username is " + username + "_YearUp");
+        int grade;
+        do {
+            System.out.println("What is your grade?");
+            grade = GradeObj.nextInt();
+            System.out.println("The inputted number must be between 1 and 100");
+        } while (grade < 1 || grade > 100);
+        if (grade % 2 == 0) {
+            System.out.println("The grade is an even number");
+        } else {
+            System.out.println("The grade is an odd number");
+        }
+        GradeObj.close();
+    }
+}
